42aea609dd76e8f0666df3aef5181bb0e1dc6b2e
bigcode <bigcode07@gmail.com>
2025-04-06 20:48:55 -0300

    feat: Mostrar productos en pantalla

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,5 +1,8 @@
 import { ProductCardComponent } from '@/products/components/product-card/product-card.component';
-import { ProductsResponse } from '@/products/interfaces/product.interface';
+import {
+  Product,
+  ProductsResponse,
+} from '@/products/interfaces/product.interface';
 import { ProductsService } from '@/products/services/products.service';
 import { Component, inject } from '@angular/core';
 
@@ -11,15 +14,16 @@ import { Component, inject } from '@angular/core';
 export class HomePageComponent {
   // Inyecta el servicio de productos usando la función `inject()` (alternativa al uso de constructor)
   productsService = inject(ProductsService);
+  products: Product[] = [];
 
   // Método que se ejecuta automáticamente cuando el componente se inicializa
   ngOnInit(): void {
     // Llama al método `getProducts()` del servicio para obtener productos desde el backend
-    this.productsService.getProducts({ limit: 5, gender: 'women' }).subscribe({
+    this.productsService.getProducts({}).subscribe({
       // Se ejecuta cuando se recibe una respuesta satisfactoria del backend
       next: (resp: ProductsResponse) => {
         // Muestra en consola la respuesta del backend (para depuración o verificación)
-        console.log('Respuesta del backend', resp);
+        this.products = resp.products;
       },
 
       // Se ejecuta si ocurre un error al realizar la petición
