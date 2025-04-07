93098cbc21fe7c248136fdea366ae35bdaea2940
bigcode <bigcode07@gmail.com>
2025-04-07 11:36:58 -0300

    feat: Servicio de paginacion

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,10 +1,9 @@
-import { ActivatedRoute } from '@angular/router';
 import { ProductCardComponent } from '@/products/components/product-card/product-card.component';
 import { ProductsService } from '@/products/services/products.service';
 import { PaginationComponent } from '@/shared/components/pagination/pagination.component';
+import { PaginationService } from '@/shared/components/pagination/pagination.service';
 import { Component, inject } from '@angular/core';
 import { rxResource, toSignal } from '@angular/core/rxjs-interop';
-import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -14,20 +13,10 @@ import { map } from 'rxjs';
 export class HomePageComponent {
   // Inyecta el servicio de productos usando la función `inject()` (alternativa al uso de constructor)
   productsService = inject(ProductsService);
-  activatedRoute = inject(ActivatedRoute);
-
-  currentPage = toSignal(
-    this.activatedRoute.queryParamMap.pipe(
-      map((params) => (params.get('page') ? +params.get('page')! : 1)),
-      map((page) => (isNaN(page) ? 1 : page))
-    ),
-    {
-      initialValue: 1,
-    }
-  );
+  paginationService = inject(PaginationService);
 
   productResource = rxResource({
-    request: () => ({ page: this.currentPage() - 1 }),
+    request: () => ({ page: this.paginationService.currentPage() - 1 }),
     loader: ({ request }) => {
       return this.productsService.getProducts({ offset: request.page * 9 });
     },
