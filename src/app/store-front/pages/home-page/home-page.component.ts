99b334faf6cb05bc75483eae72085952977150df
bigcode <bigcode07@gmail.com>
2025-04-07 10:50:32 -0300

    feat: Paginacion local sin funcionalidad

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,36 +1,22 @@
 import { ProductCardComponent } from '@/products/components/product-card/product-card.component';
-import {
-  Product,
-  ProductsResponse,
-} from '@/products/interfaces/product.interface';
 import { ProductsService } from '@/products/services/products.service';
+import { PaginationComponent } from '@/shared/components/pagination/pagination.component';
 import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-home-page',
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, PaginationComponent],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
   // Inyecta el servicio de productos usando la función `inject()` (alternativa al uso de constructor)
   productsService = inject(ProductsService);
-  products: Product[] = [];
 
-  // Método que se ejecuta automáticamente cuando el componente se inicializa
-  ngOnInit(): void {
-    // Llama al método `getProducts()` del servicio para obtener productos desde el backend
-    this.productsService.getProducts({}).subscribe({
-      // Se ejecuta cuando se recibe una respuesta satisfactoria del backend
-      next: (resp: ProductsResponse) => {
-        // Muestra en consola la respuesta del backend (para depuración o verificación)
-        this.products = resp.products;
-      },
-
-      // Se ejecuta si ocurre un error al realizar la petición
-      error: (err) => {
-        // Muestra en consola el error ocurrido al intentar obtener los productos
-        console.error('Error al obtener productos', err);
-      },
-    });
-  }
+  productResource = rxResource({
+    request: () => ({}),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({});
+    },
+  });
 }
