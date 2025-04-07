5af1044b063abae14d79e7edd8d9ec423954d2f2
bigcode <bigcode07@gmail.com>
2025-04-06 22:46:43 -0300

    feat: Generacion de pantalla de producto

--- src/app/store-front/pages/product-page/product-page.component.ts
+++ src/app/store-front/pages/product-page/product-page.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { ProductsService } from '@/products/services/products.service';
+import { Component, inject, resource } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-product-page',
   imports: [],
   templateUrl: './product-page.component.html',
 })
-export class ProductPageComponent { }
+export class ProductPageComponent {
+  activatedRoute = inject(ActivatedRoute);
+  productService = inject(ProductsService);
+
+  productIdSlug = this.activatedRoute.snapshot.params['idSlug'];
+
+  productResource = resource({
+    request: () => ({ idSlug: this.productIdSlug }),
+    loader: ({ request }) =>
+      firstValueFrom(this.productService.getProductByIdSlug(request.idSlug)),
+  });
+}
