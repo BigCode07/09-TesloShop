26364c4a842448b99e2721d53725c5b8385d2d42
bigcode <bigcode07@gmail.com>
2025-04-07 06:44:13 -0300

    feat: Pagina de productos por genero

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+import { ProductsService } from '@/products/services/products.service';
+import { ProductCardComponent } from '../../../products/components/product-card/product-card.component';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './gender-page.component.html',
 })
-export class GenderPageComponent {}
+export class GenderPageComponent {
+  route = inject(ActivatedRoute);
+  productsService = inject(ProductsService);
+
+  gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
+
+  productsResource = rxResource({
+    request: () => ({ gender: this.gender() }),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({
+        gender: request.gender,
+      });
+    },
+  });
+}
