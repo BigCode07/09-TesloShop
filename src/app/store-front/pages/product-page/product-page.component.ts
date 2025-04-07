dff2ea635e7bf55efc5267a7299b40ba8cb8c5bf
bigcode <bigcode07@gmail.com>
2025-04-06 23:16:37 -0300

    feat: Carousel de imagenes

--- src/app/store-front/pages/product-page/product-page.component.ts
+++ src/app/store-front/pages/product-page/product-page.component.ts
@@ -2,10 +2,11 @@ import { ProductsService } from '@/products/services/products.service';
 import { Component, inject, resource } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { firstValueFrom } from 'rxjs';
+import { ProductCarouselComponent } from '../../../products/components/product-carousel/product-carousel.component';
 
 @Component({
   selector: 'app-product-page',
-  imports: [],
+  imports: [ProductCarouselComponent],
   templateUrl: './product-page.component.html',
 })
 export class ProductPageComponent {
