e59eb6a7e41ec7d7164e3964d7b123dc1b740788
bigcode <bigcode07@gmail.com>
2025-04-08 10:01:04 -0300

    feat: Formulario reactivo de autenticacion

---
+++ src/app/auth/components/warning-alert/warning-alert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auth-warning-alert',
+  imports: [],
+  templateUrl: './warning-alert.component.html',
+})
+export class WarningAlertComponent {}
