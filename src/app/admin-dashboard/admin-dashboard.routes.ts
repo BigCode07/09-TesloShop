54320d4c3fec06cc7766bc1a0d298c38f062d8c1
bigcode <bigcode07@gmail.com>
2025-04-12 16:20:03 -0300

    feat: Pagina de producto por ID

--- src/app/admin-dashboard/admin-dashboard.routes.ts
+++ src/app/admin-dashboard/admin-dashboard.routes.ts
@@ -14,7 +14,7 @@ export const adminDashboardRoutes: Routes = [
         component: ProductsAdminPageComponent,
       },
       {
-        path: 'product/:id',
+        path: 'products/:id',
         component: ProductAdminPageComponent,
       },
       {
